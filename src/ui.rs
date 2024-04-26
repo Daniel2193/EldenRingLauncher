08e709613a7621dc8e8f775ea4c7ff9246ed4b87
Daniel2193 <daniel2193mc@gmail.com>
2024-04-26 23:41:25 +0200

    ModEngine2 Support

--- src/ui.rs
+++ src/ui.rs
@@ -1,4 +1,4 @@
-use std::path::Path;
+use std::{fs::rename, path::Path};
 
 pub fn draw_ui(ui: &mut imgui::Ui, opened: &mut bool) {
     ui.window(format!("{} v{}", crate::WINDOW_TITLE, crate::VERSION).as_str())
@@ -9,24 +9,41 @@ pub fn draw_ui(ui: &mut imgui::Ui, opened: &mut bool) {
         .collapsible(false)
         .opened(opened)
         .build(|| {
-            if ui.button_with_size("Start Online", [(crate::WINDOW_WIDTH as f32) - 27.0, ((crate::WINDOW_HEIGHT as f32) / 3.0) - 28.0]) {
-                let path = Path::new("./start_protected_game_original.exe");
+
+            let path_dll = Path::new("./dinput8.dll");
+            let path_dll_disabled = Path::new("./dinput8");
+
+            if ui.button_with_size("Start ModEngine2/EldenModLoader", [(crate::WINDOW_WIDTH as f32) - 30.0, ((crate::WINDOW_HEIGHT as f32) / 4.0) - 30.0]) {
+                let path = Path::new("./ModEngine2/modengine2_launcher.exe");
+
+                if !path_dll.exists() && !path_dll_disabled.exists() {
+                    _ = native_dialog::MessageDialog::new()
+                        .set_type(native_dialog::MessageType::Warning)
+                        .set_title("Warning!")
+                        .set_text("dinput8.dll not found")
+                        .show_alert();
+                    return;
+                }
+
+                if path_dll_disabled.exists() {
+                    _ = rename(path_dll_disabled, path_dll);
+                }
 
                 if !path.exists() {
                     _ = native_dialog::MessageDialog::new()
                         .set_type(native_dialog::MessageType::Error)
                         .set_title("Error!")
-                        .set_text("Could not find start_protected_game_original.exe\n\nDid you follow the installation instructions correctly?")
+                        .set_text("Could not find ModEngine2\\modengine2_launcher.exe")
                         .show_alert();
 
                     return;
                 }
 
-                if let Err(e) = std::process::Command::new(path).spawn() {
+                if let Err(e) = std::process::Command::new(path).current_dir("ModEngine2").args(["-t", "er", "-c", ".\\config_eldenring.toml"]).spawn() {
                     _ = native_dialog::MessageDialog::new()
                         .set_type(native_dialog::MessageType::Error)
                         .set_title("Error!")
-                        .set_text(format!("Could not open start_protected_game.exe\n\nError: {}", e).as_str())
+                        .set_text(format!("Could not launch modengine2_launcher.exe\n\nError: {}", e).as_str())
                         .show_alert();
                 } else {
                     std::process::exit(0);
@@ -35,7 +52,7 @@ pub fn draw_ui(ui: &mut imgui::Ui, opened: &mut bool) {
 
             ui.separator();
 
-            if ui.button_with_size("Start Seamless Coop", [(crate::WINDOW_WIDTH as f32) - 27.0, ((crate::WINDOW_HEIGHT as f32) / 3.0) - 28.0]) {
+            if ui.button_with_size("Start Seamless Coop", [(crate::WINDOW_WIDTH as f32) - 30.0, ((crate::WINDOW_HEIGHT as f32) / 4.0) - 30.0]) {
                 let path = Path::new("./launch_elden_ring_seamlesscoop.exe");
 
                 if !path.exists() {
@@ -61,8 +78,52 @@ pub fn draw_ui(ui: &mut imgui::Ui, opened: &mut bool) {
 
             ui.separator();
 
-            if ui.button_with_size("Start Offline", [(crate::WINDOW_WIDTH as f32) - 27.0, ((crate::WINDOW_HEIGHT as f32) / 3.0) - 28.0]) {
+            if ui.button_with_size("Start Online (Vanilla)", [(crate::WINDOW_WIDTH as f32) - 30.0, ((crate::WINDOW_HEIGHT as f32) / 4.0) - 30.0]) {
+                let path = Path::new("./start_protected_game_original.exe");
+
+                if path_dll.exists(){
+                    match rename(path_dll, path_dll_disabled){
+                        Ok(_) => (),
+                        Err(_) => {
+                            _ = native_dialog::MessageDialog::new()
+                                .set_type(native_dialog::MessageType::Error)
+                                .set_title("Error!")
+                                .set_text("Could not rename dinput8.dll to dinput8\n\nRefusing to launch the game")
+                                .show_alert();
+                            return;
+                        }
+                    }
+                }
+
+                if !path.exists() {
+                    _ = native_dialog::MessageDialog::new()
+                        .set_type(native_dialog::MessageType::Error)
+                        .set_title("Error!")
+                        .set_text("Could not find start_protected_game_original.exe\n\nDid you follow the installation instructions correctly?")
+                        .show_alert();
+
+                    return;
+                }
+
+                if let Err(e) = std::process::Command::new(path).spawn() {
+                    _ = native_dialog::MessageDialog::new()
+                        .set_type(native_dialog::MessageType::Error)
+                        .set_title("Error!")
+                        .set_text(format!("Could not open start_protected_game.exe\n\nError: {}", e).as_str())
+                        .show_alert();
+                } else {
+                    std::process::exit(0);
+                }
+            }
+
+            ui.separator();
+
+            if ui.button_with_size("Start Offline (Vanilla)", [(crate::WINDOW_WIDTH as f32) - 30.0, ((crate::WINDOW_HEIGHT as f32) / 4.0) - 30.0]) {
                 let path = Path::new("./eldenring.exe");
+
+                if path_dll.exists(){
+                    _ = rename(path_dll, path_dll_disabled);
+                }
 
                 if !path.exists() {
                     _ = native_dialog::MessageDialog::new()
